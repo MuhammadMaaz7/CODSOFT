b380295984f74ebc53a9f1d33977434a94dac3a2
MuhammadMaaz7 <121642049+MuhammadMaaz7@users.noreply.github.com>
2023-10-21 16:38:46 +0500

    #

---
+++ Task3.cpp
@@ -0,0 +1,79 @@
+// CODSOFT TASK 3
+#include <iostream>
+using namespace std;
+
+void display(int size, string names[], char grades[])
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Student " << i + 1 << " - Name: " << names[i] << ", Grade: " << grades[i] << endl;
+    }
+}
+
+char averageGrade(int size, char grades[])
+{
+    float sum = 0.0;
+    for (int i = 0; i < size; i++)
+    {
+        sum += static_cast<float>(grades[i]);
+    }
+    float avg = sum / size;
+    return static_cast<char>(avg);
+}
+
+char lowestGrade(int size, char grades[])
+{
+    char lowest = grades[0];
+    for (int i = 1; i < size; i++)
+    {
+        if (grades[i] > lowest)
+        {
+            lowest = grades[i];
+        }
+    }
+    return lowest;
+}
+
+char highestGrade(int size, char grades[])
+{
+    char highest = grades[0];
+    for (int i = 1; i < size; i++)
+    {
+        if (grades[i] < highest)
+        {
+            highest = grades[i];
+        }
+    }
+    return highest;
+}
+
+int main()
+{
+    int students;
+    cout << "Enter the numbr of students: ";
+    cin >> students;
+    cout << endl;
+    string names[students + 1];
+    char grades[students + 1];
+
+    cout << "Enter the student names and then there grades" << endl;
+    for (int i = 0; i < students; i++)
+    {
+        cout << "Student " << i + 1 << " Name: ";
+        cin >> names[i];
+        cout << "Student " << i + 1 << " Grade: ";
+        cin >> grades[i];
+    }
+
+    display(students, names, grades);
+
+    char average = averageGrade(students, grades);
+    char lowest = lowestGrade(students, grades);
+    char highest = highestGrade(students, grades);
+
+    cout << "Average Grade: " << average << endl;
+    cout << "Highest Grade: " << highest << endl;
+    cout << "Lowest Grade: " << lowest << endl;
+
+    return 0;
+}
